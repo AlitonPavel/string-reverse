9cf7ff08ba82d47143e16cf9379b22fdf7cf84bf
rpy <pasha-rogov@yandex.ru>
2026-09-09 17:02:21 +0300

    init library

---
+++ src/Reverser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rpy\StringReverser;
+
+class Reverser
+{
+    /**
+     * Переворачивает строку задом наперед.
+     * Поддерживает кириллицу (UTF-8).
+     */
+    public static function reverse(string $text): string
+    {
+        // Используем mb_strrev, так как встроенный strrev ломает кириллицу
+        // Если mb_string не установлен, это простой аналог:
+        $length = mb_strlen($text, 'UTF-8');
+        $reversed = '';
+
+        for ($i = $length - 1; $i >= 0; $i--) {
+            $reversed .= mb_substr($text, $i, 1, 'UTF-8');
+        }
+
+        return $reversed;
+    }
+}
